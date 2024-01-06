14460761d7a108ab637cbba4c59478298239f202
Rupesh Gawas <Rupesh Gawas@ho.rnt.ai>
2024-01-06 14:11:24 +0530

    game

--- blog_app_api/src/main/java/com/rupesh/blog/config/SecurityConfig.java
+++ blog_app_api/src/main/java/com/rupesh/blog/config/SecurityConfig.java
@@ -13,9 +13,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http
-		.cors()
+		.csrf()
 		.disable()
-		.authorizeRequests()
+		.authorizeHttpRequests()
 		.anyRequest()
 		.authenticated()
 		.and()

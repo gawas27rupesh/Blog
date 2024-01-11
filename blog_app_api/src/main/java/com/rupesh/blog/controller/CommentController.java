e0babd345c51a66979d1d7fb65057bfe69aa41ea
Rupesh Vaman Gawas <rupeshgawas01@gmail.com>
2024-01-11 09:35:58 +0530

    comment controller

---
+++ blog_app_api/src/main/java/com/rupesh/blog/controller/CommentController.java
@@ -0,0 +1,37 @@
+package com.rupesh.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rupesh.blog.dto.ApiResponse;
+import com.rupesh.blog.dto.CommentDto;
+import com.rupesh.blog.services.CommentService;
+
+@RestController
+@RequestMapping("/api/comment")
+public class CommentController {
+
+	@Autowired
+	private CommentService commentService;
+
+	@PostMapping("/post/{postId}/comments")
+	public ResponseEntity<CommentDto> createComment(@RequestBody CommentDto commentDto, @PathVariable Integer postId) {
+
+		CommentDto createComment = this.commentService.createComment(commentDto, postId);
+		return new ResponseEntity<CommentDto>(createComment, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/comments/{commenrId}")
+	public ResponseEntity<ApiResponse> deleteComment(@PathVariable Integer commentId) {
+
+		this.commentService.deleteComment(commentId);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("Comment Deleted Successfully", true), HttpStatus.OK);
+	}
+}
